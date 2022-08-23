96580e9c069db5278ee6eab39ff30b616c75e5f7
Andrey Vasilyev <avasilyev.gsg@gmail.com>
2022-08-23 15:27:21 +0300

    initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+package netology.ru.StreamNumbers;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+
+        Collections.sort(intList);
+
+        for (int num : intList) {
+            if (num > 0 && num % 2 == 0) {
+                System.out.println(num);
+            }
+        }
+    }
+}
